c3a4ea10cd36ba0959887a18fe3c6d035de4a637
Zubyk Maksym <ipz224_zmr@student.ztu.edu.ua>
2024-01-27 12:32:30 +0200

    фікс помилки   Uncaught SyntaxError: Identifier 'title' has already been declared та зміна стилю для div

--- jsdemo/js/index.js
+++ jsdemo/js/index.js
@@ -33,12 +33,12 @@ const user = new User('admin', '123456');
 
 let panelParams = {
     //width : 300,
-    title : 'Title',
+    title : 'Title', 
     height : 200,
     age : 30
 };
 
-let {title, height} = panelParams;
+let {title: panelTitle, height} = panelParams;
 let [id, title, count, price] = [1, 'title', 10, 10,5];
 let x = 1;
 let y = 2;
@@ -51,6 +51,7 @@ function func2(a, b, c) {
     console.log(b);
     console.log(c);
 }
+
 /*let arr2 = [1,2,3,4,5];
 func2(...arr2);*/
 
